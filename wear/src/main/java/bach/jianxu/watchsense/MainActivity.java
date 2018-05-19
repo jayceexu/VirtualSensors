fa687ed84e5a9393fc833c21cf8e5862b858d9e0
jayceexu <xujaycee@gmail.com>
2018-05-19 19:54:11 -0400

    add motion sensors

--- wear/src/main/java/bach/jianxu/watchsense/MainActivity.java
+++ wear/src/main/java/bach/jianxu/watchsense/MainActivity.java
@@ -1,19 +1,36 @@
 package bach.jianxu.watchsense;
 
 import android.os.Bundle;
+import android.support.wear.widget.BoxInsetLayout;
 import android.support.wearable.activity.WearableActivity;
+import android.support.wearable.view.DotsPageIndicator;
+import android.support.wearable.view.GridViewPager;
+import android.support.wearable.view.WatchViewStub;
 import android.widget.TextView;
 
 public class MainActivity extends WearableActivity {
-
-    private TextView mTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextView = (TextView) findViewById(R.id.text);
+        WatchViewStub stub = findViewById(R.id.watch_view_stub);
+
+//        final GridViewPager pager = findViewById(R.id.pager);
+//        pager.setAdapter(new SensorFragmentPagerAdapter(getFragmentManager()));
+//        stub.addView(pager);
+
+        stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
+            @Override public void onLayoutInflated(WatchViewStub stub) {
+                final GridViewPager pager = findViewById(R.id.pager);
+                pager.setAdapter(new SensorFragmentPagerAdapter(getFragmentManager()));
+
+                DotsPageIndicator indicator = findViewById(R.id.page_indicator);
+                indicator.setPager(pager);
+            }
+        });
+
 
         // Enables Always-on
         setAmbientEnabled();
