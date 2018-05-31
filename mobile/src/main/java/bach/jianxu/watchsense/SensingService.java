4cd243f79ffc656ec6d260dfbd151a165e913b23
jayceexu <xujaycee@gmail.com>
2018-05-31 11:46:37 -0400

    1. change env 2. add new API: SensorEventInjector    so that this project only works for our customized Android

--- mobile/src/main/java/bach/jianxu/watchsense/SensingService.java
+++ mobile/src/main/java/bach/jianxu/watchsense/SensingService.java
@@ -3,8 +3,15 @@ package bach.jianxu.watchsense;
 
 import android.app.Service;
 import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventInjector;
+import android.hardware.SensorEventListener;
+
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
@@ -23,10 +30,13 @@ import com.google.android.gms.wearable.Wearable;
 public class SensingService extends Service implements
         DataApi.DataListener,
         GoogleApiClient.ConnectionCallbacks,
-        MessageApi.MessageListener {
+        MessageApi.MessageListener,
+        SensorEventInjector {
 
     final static String TAG = "AccessibilityService";
     private GoogleApiClient mGoogleApiClient;
+    private SensorManager mSensorManager;
+
     private static final String MESSAGE = "/message";
     private static final String SEPARATOR = "||";
     private static int id = 1;
@@ -37,6 +47,10 @@ public class SensingService extends Service implements
                 .addConnectionCallbacks(this)
                 .build();
         mGoogleApiClient.connect();
+
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        Sensor sensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mSensorManager.registerInjector(this, sensor, SensorManager.SENSOR_DELAY_NORMAL, 1);
 
         Log.i(TAG, "Creating..........");
         super.onCreate();
@@ -54,6 +68,19 @@ public class SensingService extends Service implements
         return super.registerReceiver(receiver, filter);
     }
 
+    @Override
+    public void onSensorReceived(SensorEvent sensorEvent) {
+        Log.i(TAG, "onSensorReceived.........");
+
+        String str = "";
+
+        for (int i = 0; i < sensorEvent.values.length; ++i) {
+            sensorEvent.values[i] -= 1;
+            str += sensorEvent.values[i] + ", ";
+        }
+        Log.i(TAG, "onSensorReceived: values " + str);
+
+    }
 
     private void sendMessage(final String path, final String text) {
         new Thread(new Runnable() {
@@ -87,6 +114,10 @@ public class SensingService extends Service implements
     @Override
     public void onMessageReceived(MessageEvent messageEvent) {
         Log.i(TAG, "~~~~~~~~~~~~~Received msg: " + new String(messageEvent.getData()));
+        // Receiving the motion sensor data
+
+
+
     }
 
 
